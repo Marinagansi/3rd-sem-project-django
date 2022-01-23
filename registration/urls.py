98d9c1ef859b67d1f7e78c83cc72d424c99d7e06
Ashok-gc <ashokgc.np@gmail.com>
2022-01-23 22:08:36 +0545

    update2

--- registration/urls.py
+++ registration/urls.py
@@ -19,9 +19,7 @@ urlpatterns = [
             path('contact',views.contact),
             path('about_us',views.about_us),
             path('hotel',views.hotel),
-<<<<<<< HEAD
-            path('logout',views.logout_view),
-=======
+
             path('forget_password',views.forget_password),
             path('forget_username',views.forget_username),
             path('having_trouble',views.having_trouble),
@@ -30,13 +28,8 @@ urlpatterns = [
             path('partneracc_signin2',views.partneracc_signin2),
             path('partneracc_signup',views.partneracc_signup),
             path('partneracc_signup2',views.partneracc_signup2),
-            path('partneracc_signup3',views.partneracc_signup3),
+            path('partneracc_signup3',views.partneracc_signup3)       
 
 
->>>>>>> 5ce1095e2daa8dc05f008ec7ce864730f146a66f
-            
-          
-           
-       
-   
+
 ]
