e5cf8cc01e73bba483de3b0b548a5b12c318fa82
Ashok-gc <ashokgc.np@gmail.com>
2022-02-02 21:12:32 +0545

    update

--- hotel/forms.py
+++ hotel/forms.py
@@ -10,3 +10,16 @@ class HotelForm2(forms.ModelForm):
     class Meta:
      model = Hotel
      fields = ("room_type","smoking_policy","no_of_rooms","Beds","no_of_guest")
+
+class HotelFormStepOne(forms.ModelForm):
+    class Meta:
+     model = Hotel
+     fields = ("hotel_name","contact_name","phonenumber","hotel_address","hotel_region")
+
+
+class HotelFormStepTwo(forms.ModelForm):
+    class Meta:
+     model = Hotel
+     fields = ("room_type","smoking_policy","no_of_rooms","Beds","no_of_guest")
+
+    
--- hotel/urls.py
+++ hotel/urls.py
@@ -8,7 +8,9 @@ urlpatterns = [
     # path('add_hotel', views.hotel_form),
     # re_path(r'^hotel/hotel2/(?hotel_name>\d+)/$', views.step2, name='add_hotel2')
     # path('update_hotel/<str:hotel_name',views.update_hotel,name="hotel2")
-    path('add_hotel', views.step1),
+    path('add_hotel', views.stepOneSubmit),
+    path('add_hotel/<int:p_id>', views.stepTwoSubmit),
+
     path('hotel2',views.step2,name='step2')
     
     
